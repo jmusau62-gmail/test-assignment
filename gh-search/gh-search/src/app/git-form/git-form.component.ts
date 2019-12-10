40c5f028b00247a34bdbedea09e4b1376c2f3744
jmusau62-gmail <jmusau62@gmail.com>
2019-12-10 10:48:55 +0300

    added changes on import

--- gh-search/gh-search/src/app/git-form/git-form.component.ts
+++ gh-search/gh-search/src/app/git-form/git-form.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import {User} from '../user'
+
 @Component({
   selector: 'app-git-form',
   templateUrl: './git-form.component.html',
   styleUrls: ['./git-form.component.css']
 })
 export class GitFormComponent implements OnInit {
+  user:User;
+	username: string;
+  searchGithubService:any;
+  public showInput = true;
+  public showData = false;
 
-  constructor() { }
+  constructor() {
+    this.searchGithubService.getUserData(this.username);
+    this.searchGithubService.getUserData(this.username);
+      this.showInput=false;
+      this.showData=true;
+      this.showData= false;
+   }
 
   ngOnInit() {
   }
